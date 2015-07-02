5ac45d3fe7339d90146c521156e07a0dd44b4d57
Joseph Leon <joseph.leon.9@gmail.com>
2015-07-02 13:05:10 -0400

    Add values to the countries variable, add askQuestion function inside viewDidLoad, create askQuestion func and call setImage on the buttons.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -14,9 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+ 
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
